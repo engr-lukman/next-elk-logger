0e07fc79ee0bceb8ae49f46327b45b2f4ac63eeb
Lukman <engr.lukman@gmail.com>
2023-03-20 19:10:59 +0600

    Multi stage split

--- client/src/pages/api/hello.ts
+++ client/src/pages/api/hello.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import logger from "utils/logger";
+
 type Data = {
   name: string;
 };
@@ -9,5 +11,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  logger.error(`A error message from api/hello`);
+
   res.status(200).json({ name: "John Doe" });
 }
